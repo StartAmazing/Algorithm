4a66fab7c8d46ee69a55aab4b4bcc39cae943204
tom_glb <goodMorning@163.com>
2019-10-28 23:55:17 +0800

    滑动窗口

--- src/com/ll/zs/advance/advanced_class_01/mytest/SlidingWindowMaxArray.java
+++ src/com/ll/zs/advance/advanced_class_01/mytest/SlidingWindowMaxArray.java
@@ -16,19 +16,18 @@ public class SlidingWindowMaxArray {
         int[] res = new int[arr.length - w + 1];
         int index = 0;
         for(int i = 0 ; i < arr.length; i++){
-            while (!qmax.isEmpty() && arr[qmax.getLast()]< arr[i]){
+            while (!qmax.isEmpty() && arr[qmax.getLast()]< arr[i]){     //如果新增的数比原来窗口中最右边的数还要大的时候
                 qmax.removeLast();
             }
-            qmax.addLast(arr[i]);
-            if(qmax.getFirst() == i - w ){
+            qmax.addLast(arr[i]);   //往队列中存入索引值
+            if(qmax.getFirst() == i - w ){      //当前索引i到达的位置与第一个qmax最左边的位置之差为窗口大小的时候，进行缩减
                 qmax.removeFirst();
             }
-            if(i >= w - 1){
+            if(i >= w - 1){     //窗口是否形成，不管是否进行了缩减，自从第一次窗口中含有w个数的时候，往结果集中放入结果
                 res[index ++] = arr[qmax.getFirst()];
             }
         }
         return  res;
     }
 
-    private static void peekLast()
 }
