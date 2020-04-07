a3be34e4437767df5534922a3afef88c5b32e1ed
Stop Working, Start Amazing! <41231730+StartAmazing@users.noreply.github.com>
2020-04-07 09:14:49 +0800

    旋转数组

--- src/com/ll/zs/matrix/RotateMatrix.java
+++ src/com/ll/zs/matrix/RotateMatrix.java
@@ -40,7 +40,7 @@ public class RotateMatrix {
         int[][] m = {{1,2,3,4},{5,6,7,8},{9,10,11,12,13},{14,15,16,17}};
         printMatrix(m);
         rotate(m);
-        System.out.println("================");
+        System.out.println("=================");
         printMatrix(m);
     }
 }
