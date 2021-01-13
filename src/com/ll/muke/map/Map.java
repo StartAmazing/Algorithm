a2e46ca429a41dfb824132f4ba4588a206247d27
Stop Working, Start Amazing! <41231730+StartAmazing@users.noreply.github.com>
2021-01-13 10:17:04 +0800

    添加注释

--- src/com/ll/muke/map/Map.java
+++ src/com/ll/muke/map/Map.java
@@ -1,12 +1,27 @@
 package com.ll.muke.map;
 
+
+/**
+ * map接口定义
+ */
 public interface Map <K, V>{
 
+    /* 添加一个元素 */
     void add(K key, V value);
+
+    /* 删除一个元素 */
     V remove(K key);
+
+    /* 判断是否已包含这个key */
     boolean contains(K key);
-    V get(K key);
+
+    /* 为已有的key设置新值 */
     void set(K key, V newValue);
+
+    /* 获取当前元素个数 */
     int getSize();
+
+    /* 判断是否为空map */
     boolean isEmpty();
+
 }
