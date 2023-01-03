3a82afc570f3b33e26583ed769fe19c7c8b06388
liuliang <1966702922@qq.com>
2023-01-03 18:40:24 +0800

    two pointer follow up

--- src/com/ll/nine_chapter/advance/a_follow_up/TwoPointer.java
+++ src/com/ll/nine_chapter/advance/a_follow_up/TwoPointer.java
@@ -1,6 +1,8 @@
 package com.ll.nine_chapter.advance.a_follow_up;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class TwoPointer {
@@ -64,10 +66,96 @@ public class TwoPointer {
      * @return: A string denote the minimum window, return "" if there is no such a string
      */
     public String minWindow(String source, String target) {
-        return "";
+        if(source.length() < target.length()) {
+            return "";
+        }
+
+        int[] sourceMap = new int[256];
+        int[] targetMap = new int[256];
+
+        initTargetMap(target, targetMap);
+        int left = 0, right = 0;
+        String res = "";
+        while (right < source.length()) {
+            while(!checkValid(sourceMap, targetMap) && right < source.length()) {
+                sourceMap[source.charAt(right++)]++;
+            }
+
+            while (checkValid(sourceMap, targetMap)) {
+                if (res.equals("")) {
+                    res = source.substring(left, right);
+                } else {
+                    res = res.length() > right - left ? source.substring(left, right) : res;
+                }
+                sourceMap[source.charAt(left++)]--;
+            }
+
+        }
+
+        return res;
+    }
+
+    private void initTargetMap(String s, int[] resMap) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            resMap[chars[i]]++;
+        }
+    }
+
+    private boolean checkValid(int[] sourceMap, int[] targetMap) {
+        for (int i = 0; i < 256; i++) {
+            if(sourceMap[i] < targetMap[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @link https://www.lintcode.com/problem/386/
+     * @param s: A string
+     * @param k: An integer
+     * @return: An integer
+     */
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if(k == 0) {
+            return 0;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, right = 0;
+        int res = 0;
+        while(right < s.length()) {
+            while(right < s.length() && map.keySet().size() < k) {
+                map.putIfAbsent(s.charAt(right), 0);
+                map.put(s.charAt(right), map.get(s.charAt(right)) + 1);
+                right++;
+            }
+
+            while(right < s.length() && map.keySet().contains(s.charAt(right))) {
+                map.put(s.charAt(right), map.get(s.charAt(right)) + 1);
+                right++;
+            }
+
+            res = Math.max(res, right - left);
+
+            map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+            if(map.get(s.charAt(left)) == 0) {
+                map.remove(s.charAt(left));
+            }
+            left++;
+        }
+
+        return res;
     }
 
 
     public static void main(String[] args) {
+        TwoPointer dto = new TwoPointer();
+        String source = "eqgkcwGFvjjmxutystqdfhuMblWbylgjxsxgnoh";
+        String target = "abcdd";
+
+        System.out.println(dto.lengthOfLongestSubstringKDistinct(source, 16));
     }
 }
